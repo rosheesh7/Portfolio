112580b456e90ee0eacfabccdacf89557bda06fc
rosheesh7 <rosissharma87@gmail.com>
2025-10-07 10:35:00 +0545

    Complete About Section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,23 @@
 import "./About.css";
+const card_data = [
+  {
+    id: 1,
+    title: "Education",
+    description:
+      "Bachelor of Science in Computer Science and Information Technology  [Bsc.CSIT]",
+  },
+  {
+    id: 2,
+    title: "Journey",
+    description: `I specialize in building modern, sleek, and responsive user interfaces with React.js, and I’m also deeply interested in the evolving world of Artificial Intelligence and Machine Learning.`,
+  },
+  {
+    id: 3,
+    title: "Interests",
+    description: `Music and sports keep me inspired — I play guitar, drums, ukulele,
+        and enjoy football. Creativity fuels everything I do.`,
+  },
+];
 const About = () => {
   return (
     <>
@@ -11,6 +30,17 @@ const About = () => {
             user experiences. My work focuses on clean code, modern UI design,
             and creating interactive web interfaces that delight users.
           </span>
+        </div>
+
+        <div className="about-card-container">
+          {card_data.map((card) => {
+            return (
+              <div className="about-card" key={card.id}>
+                <h1>{card.title}</h1>
+                <span>{card.description}</span>
+              </div>
+            );
+          })}
         </div>
       </section>
     </>
